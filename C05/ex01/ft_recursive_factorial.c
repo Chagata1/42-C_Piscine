8e4009c8a35f763d44afccc842cce533fa2e98d1
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:14:12 +0000

    Updated

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 12:42:11 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/07 12:51:19 by ryayla           ###   ########.fr       */
