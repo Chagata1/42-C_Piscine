3c5ac5882d537fcb1963f748dffdcbc946a1468e
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 11:51:30 +0000

    Update ft_strcat.c

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -32,8 +32,8 @@ char	*ft_strcat(char *dest, char *src)
 /*
  int    main()
  {
-     char src[] = "Benguerir";
-     char dest[] = "1337 ";
+     char src[] = "Mahmut";
+     char dest[] = "Tuncer";
      printf("%s", ft_strcat(dest, src));
  }
  */
