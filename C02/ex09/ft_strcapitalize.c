e3455f931b08eac818809ceaf8a23b227c2a5eaa
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:11:51 +0000

    Updated

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:47:34 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/01 12:53:56 by ryayla           ###   ########.fr       */
