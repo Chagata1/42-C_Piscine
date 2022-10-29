38dd9983842148d02ab0abd0da53ac2a2fa7f58d
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:13:57 +0000

    Updated

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 12:30:18 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/10 15:15:20 by ryayla           ###   ########.fr       */
