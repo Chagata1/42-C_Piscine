894aaf24ce3e678e1a648db49350bb77340ce50e
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:11:14 +0000

    Updated

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 14:46:44 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/03 12:21:30 by ryayla           ###   ########.fr       */
