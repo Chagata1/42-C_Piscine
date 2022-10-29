cdd7506aad5c5db452b3e59e7b368f447a5d8ed6
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:06:12 +0000

    Updated

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 12:06:43 by ryayla            #+#    #+#             */
 /*   Updated: 2022/07/25 12:08:18 by ryayla           ###   ########.fr       */
