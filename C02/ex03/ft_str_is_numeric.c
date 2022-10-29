fe2f76c685f8c7cecc971dd4912a55d97f97f80c
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:09:32 +0000

    Updated

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 17:29:48 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/01 11:54:50 by ryayla           ###   ########.fr       */
