7ae1bfa68e8bdf0fdfab0d50f329ad5301fcd16f
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:13:12 +0000

    Updated

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 11:53:49 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/03 11:53:57 by ryayla           ###   ########.fr       */
