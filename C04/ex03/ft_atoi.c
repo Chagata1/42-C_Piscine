d7503b8b66779ed06be45149e67aed568d44a786
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:13:35 +0000

    Updated

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 11:58:11 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/03 11:58:19 by ryayla           ###   ########.fr       */
