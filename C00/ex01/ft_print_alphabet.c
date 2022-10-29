bf7ce62fd6df96cbca567a413129c41e72b23f2e
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:02:18 +0000

    Updated

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 19:00:35 by ryayla            #+#    #+#             */
 /*   Updated: 2022/07/21 19:09:45 by ryayla           ###   ########.fr       */
