2bedf3b224b27a724aab6b46b132f7429bedcae7
Chagatai <100884204+Chagata1@users.noreply.github.com>
2022-10-29 12:11:33 +0000

    Updated

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ryayla <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ryayla <ryayla@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 16:40:02 by ryayla            #+#    #+#             */
 /*   Updated: 2022/08/03 12:22:52 by ryayla           ###   ########.fr       */
